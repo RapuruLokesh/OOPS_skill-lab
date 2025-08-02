3ab4171718e05809f690ef972cc0dd3ca901996b
RapuruLokesh <2420030534@klh.edu.in>
2025-08-02 10:41:46 +0530

    Add files via upload

---
+++ sumofdigits.java
@@ -0,0 +1,30 @@
+package LOKESH;
+import java.util.Scanner;
+
+public class sumofdigits{
+    
+    public static int sumOfDigits(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;  
+            num /= 10;        
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        
+        while (number > 9) {
+            number = sumOfDigits(number);
+        }
+
+        System.out.println("Sum of digits reduced to a single digit: " + number);
+
+        scanner.close();
+    }
+}
