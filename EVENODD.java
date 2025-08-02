ac342efa02862e144419d11c35552ab0074a8a81
RapuruLokesh <2420030534@klh.edu.in>
2025-08-02 10:38:07 +0530

    Add files via upload

---
+++ EVENODD.java
@@ -0,0 +1,23 @@
+package LOKESH;
+
+import java.util.Scanner;
+
+public class EVENODD {
+public static void main(String[] args) {
+ 
+ Scanner sc = new Scanner(System.in);
+ 
+ int num = sc.nextInt();
+ 
+ 
+ if(num%2==0) {
+  
+  System.out.println("EVEN NUMBER");
+ } else {
+  System.out.println("ODD NUMBER");
+ }
+}
+
+ 
+}
+
