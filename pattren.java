822e0fb79aacd57534bd7c24fdbb20caad2d9761
RapuruLokesh <2420030534@klh.edu.in>
2025-08-02 10:39:34 +0530

    Add files via upload

---
+++ pattren.java
@@ -0,0 +1,22 @@
+package LOKESH;
+
+
+import java.util.Scanner;
+
+public class pattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the starting number: ");
+        int start = scanner.nextInt();
+
+        for (int row = 0; row < start; row++) {
+            for (int num = start; num > row; num--) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
